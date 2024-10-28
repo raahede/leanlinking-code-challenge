57bd3233feeb72496601258613f0f7ba93f10fa4
Frej Raahede Nielsen <frejraahede@gmail.com>
2024-10-28 13:50:22 +0100

    Add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettierPlugin from 'eslint-plugin-prettier'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,9 +17,11 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'prettier': prettierPlugin
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...prettierPlugin.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
